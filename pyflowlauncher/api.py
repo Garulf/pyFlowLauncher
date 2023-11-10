9ae9789ed1889ebca91fdede23772e68866c7f80
Garulf <535299+Garulf@users.noreply.github.com>
2023-11-10 01:47:53 -0500

    Use private function

--- pyflowlauncher/api.py
+++ pyflowlauncher/api.py
@@ -1,53 +1,57 @@
-from .jsonrpc import JsonRPCRequest
+from typing import Optional
+from .result import JsonRPCAction
 
 NAME_SPACE = 'Flow.Launcher'
 
 
-def change_query(query: str, requery: bool = False) -> JsonRPCRequest:
+def _send_action(method: str, *parameters) -> JsonRPCAction:
+    return {"method": f"{NAME_SPACE}.{method}", "parameters": parameters}
+
+
+def change_query(query, requery=False) -> JsonRPCAction:
     """Change the query in Flow Launcher."""
-    return {"method": f"{NAME_SPACE}.ChangeQuery", "parameters": [query, requery]}
+    return _send_action("ChangeQuery", query, requery)
 
 
-def shell_run(command: str) -> JsonRPCRequest:
+def shell_run(command: str, filename: str = 'cmd.exe') -> JsonRPCAction:
     """Run a shell command."""
-    return {"method": f"{NAME_SPACE}.ShellRun", "parameters": [command]}
+    return _send_action("ShellRun", command, filename)
 
 
-def close_app() -> JsonRPCRequest:
+def close_app() -> JsonRPCAction:
     """Close Flow Launcher."""
-    return {"method": f"{NAME_SPACE}.CloseApp", "parameters": []}
+    return _send_action("CloseApp")
 
 
-def hide_app() -> JsonRPCRequest:
+def hide_app() -> JsonRPCAction:
     """Hide Flow Launcher."""
-    return {"method": f"{NAME_SPACE}.HideApp", "parameters": []}
+    return _send_action("HideApp")
 
 
-def show_app() -> JsonRPCRequest:
+def show_app() -> JsonRPCAction:
     """Show Flow Launcher."""
-    return {"method": f"{NAME_SPACE}.ShowApp", "parameters": []}
+    return _send_action("ShowApp")
 
 
-def show_msg(title: str, sub_title: str, ico_path: str = "") -> JsonRPCRequest:
+def show_msg(title, sub_title, ico_path="") -> JsonRPCAction:
     """Show a message in Flow Launcher."""
-    return {"method": f"{NAME_SPACE}.ShowMsg", "parameters": [title, sub_title, ico_path]}
+    return _send_action("ShowMsg", title, sub_title, ico_path)
 
 
-def open_setting_dialog() -> JsonRPCRequest:
+def open_setting_dialog() -> JsonRPCAction:
     """Open the settings window in Flow Launcher."""
-    return {"method": f"{NAME_SPACE}.OpenSettingDialog", "parameters": []}
+    return _send_action("OpenSettingDialog")
 
 
-def start_loading_bar() -> JsonRPCRequest:
+def start_loading_bar() -> JsonRPCAction:
     """Start the loading bar in Flow Launcher."""
-    return {"method": f"{NAME_SPACE}.StartLoadingBar", "parameters": []}
+    return _send_action("StartLoadingBar")
 
 
-def stop_loading_bar() -> JsonRPCRequest:
+def stop_loading_bar() -> JsonRPCAction:
     """Stop the loading bar in Flow Launcher."""
-    return {"method": f"{NAME_SPACE}.StopLoadingBar", "parameters": []}
+    return _send_action("StopLoadingBar")
 
 
-def reload_plugins() -> JsonRPCRequest:
+def reload_plugins() -> JsonRPCAction:
     """Reload the plugins in Flow Launcher."""
-    return {"method": f"{NAME_SPACE}.ReloadPlugins", "parameters": []}
