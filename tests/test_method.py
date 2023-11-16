8e445004c574903d08aad2d840021958a36d17d5
Garulf <535299+Garulf@users.noreply.github.com>
2023-11-16 00:34:01 -0500

    Add tests for method module

---
+++ tests/test_method.py
@@ -0,0 +1,25 @@
+from pyflowlauncher.method import Method
+from pyflowlauncher.result import Result
+
+
+def test_add_results():
+    class TestMethod(Method):
+        def __call__(self, *args, **kwargs):
+            result = Result("title", "subtitle", "icon.png")
+            self.add_result(result)
+
+    method = TestMethod()
+    method()
+    assert method._results == [Result("title", "subtitle", "icon.png")]
+
+
+def test_return_results():
+    result = Result("title", "subtitle", "icon.png")
+
+    class TestMethod(Method):
+        def __call__(self, *args, **kwargs):
+            self.add_result(result)
+            return self.return_results()
+
+    method = TestMethod()
+    assert method() == {"result": [result.as_dict()]}
