a8aa9a8f573dd76747d85f6d4f23670e6b08b469
Garulf <535299+Garulf@users.noreply.github.com>
2023-11-15 21:17:34 -0500

    Fix attribute error

--- pyflowlauncher/shared.py
+++ pyflowlauncher/shared.py
@@ -5,8 +5,7 @@ _logger = logging.getLogger(__name__)
 
 
 class PyFlowLauncherObject:
-    _logger = logging.getLogger(__qualname__)
+    _logger = None
 
     def __init_subclass__(cls) -> None:
-        cls._logger = logging.getLogger(f"{cls.__module__}.{cls.__qualname__}")
-
+        cls._logger = logging.getLogger(f"{cls.__module__}.{cls.__name__}")
