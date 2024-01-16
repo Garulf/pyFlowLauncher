fd49f2a0d59511c9da8ee2fb1ad954071f7aa3fe
Garulf <535299+Garulf@users.noreply.github.com>
2024-01-16 03:09:11 -0500

    Add public method for registering exception callbacks

--- pyflowlauncher/plugin.py
+++ pyflowlauncher/plugin.py
@@ -46,6 +46,10 @@ class Plugin:
         """Register a method to be called when the plugin is run."""
         return self.on_method(method)
 
+    def add_exception_handler(self, exception: Exception, handler: Callable[..., Any]) -> None:
+        """Add exception handler to be called when an exception is raised in a method."""
+        self._event_handler.add_exception_handler(exception, handler)
+
     @property
     def settings(self) -> dict:
         if self._settings is None:
